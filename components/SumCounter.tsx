4bbfb00bd52724803c621d179cdc0511f92d7ca1
hironobugt <hironobugt@gmail.com>
2025-05-14 16:43:33 +0900

    subtraction

--- components/SumCounter.tsx
+++ components/SumCounter.tsx
@@ -2,19 +2,26 @@
 
 import { useState } from 'react'
 import { sum } from '../lib/sum'
+import { subtract } from '../lib/subtract'
 
 export function SumCounter() {
   const [count, setCount] = useState(0)
-
-  const handleClick = () => {
+ const handleAdd = () => {
     setCount((prev) => sum(prev, 1))
+  }
+
+  const handleSubtract = () => {
+    setCount((prev) => subtract(prev, 1))
   }
 
   return (
     <div>
       <h1>Sum Example</h1>
       <p>Current total: {count}</p>
-      <button onClick={handleClick}>Add 1</button>
+      <button onClick={handleAdd}>Add 1</button>
+      <button onClick={handleSubtract} style={{ marginLeft: 8 }}>
+        Subtract 1
+      </button>
     </div>
   )
 }
